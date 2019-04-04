5c7c0020bba0fe34054207182c935a7bc9537268
Unknown <Cyyke.cs@gmail.com>
2019-04-04 15:20:07 -0700

    Apply data/ui adjustment patch

--- src/app/albums/album.model.ts
+++ src/app/albums/album.model.ts
@@ -5,4 +5,18 @@ export interface Album {
     genre: string;
     price: number;
     currency: string;
+    on_sale: boolean;
+    year: number;
+    release_date: string,
+    recording_location: string;
+    duration: string;
+    url: string;
+    tracks: Track[];
+}
+
+export interface Track {
+    id: number;
+    track_number: number;
+    title: string,
+    length: string
 }
