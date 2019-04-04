31197dd860e1d9aaacec78e1acf89c52e43c56dc
Unknown <Cyyke.cs@gmail.com>
2019-04-04 15:28:30 -0700

    Apply data/ui adjustment patch

--- src/app/albums/album-list/album-list.component.ts
+++ src/app/albums/album-list/album-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Album } from '../album.model';
+import { ALBUMS } from '../albums.data';
 
 @Component({
   selector: 'app-album-list',
@@ -17,33 +18,7 @@ export class AlbumListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.albums = [
-      {
-        id: 1,
-        artist: "Tremonti",
-        album_name: "Dust",
-        genre: "Pop/Rock",
-        price: 12,
-        currency: "USD"
-      },
-      {
-        id: 2,
-        artist: "Bon Jovi",
-        album_name: "7800 Fahrerenheit",
-        genre: "Pop/Rock",
-        price: 7,
-        currency: "EUR"
-      },
-      {
-        id: 3,
-        artist: "The Beatles",
-        album_name: "The White Album",
-        genre: "Pop/Rock",
-        price: 12,
-        currency: "EUR"
-      }
-    ];
-
+    this.albums = ALBUMS;
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -6,5 +6,5 @@ import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit } from "@
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "my-angular-albums";
+  title = "My Angular Albums";
 }
