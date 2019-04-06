b63346612432023477032869d17b25f659e215b7
Judy Lipinski <judy@karmoxie.com>
2019-04-05 22:56:04 -0400

    updated path to img - and fixed changearray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,46 @@
-import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit } from "@angular/core";
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  OnInit
+} from "@angular/core";
 
-import { Album } from './albums/album.model';
-import { ALBUMS } from './albums/albums.data';
+import { Album } from "./albums/album.model";
+import { ALBUMS } from "./albums/albums.data";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styleUrls: ["./app.component.css"]
   // Un-comment to prevent UI from updating
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'My Angular Albums';
+  title = "My Angular Albums";
   albumsArray: Album[];
   numbers: number[];
 
   titleCounter = 1;
 
-  constructor(private cdr: ChangeDetectorRef) { }
+  constructor() {}
 
   ngOnInit(): void {
     this.albumsArray = ALBUMS;
-    this.numbers = [1, 2, 3];
 
-    console.log(JSON.stringify(this.albumsArray, null, 4));
+    console.log(JSON.stringify(this.albumsArray));
 
-    const interval = setInterval(() => {
+    const intervalId = setInterval(() => {
       this.titleCounter++;
 
-      // Array manipulation, log results, NO UI Reflection
-      this.numbers.push(Math.floor(Math.random() * 10));
+      this.albumsArray[0].price = this.albumsArray[0].price + 10;
+
       console.log(this.numbers);
 
-      this.albumsArray.forEach((album) => album.price += 10);
-
-      console.log(JSON.stringify(this.albumsArray, null, 4));
-
-      // Un-comment to show changes in UI
-      // this.cdr.detectChanges();
+      this.albumsArray.forEach(album => (album.price += 10));
     }, 2000);
 
     setTimeout(() => {
-      clearInterval(interval);
+      clearInterval(intervalId);
     }, 6000);
   }
 }
