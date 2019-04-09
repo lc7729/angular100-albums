9c8c42c18da62d6c5893137a7511f35e188519e2
Judy Lipinski <judy@karmoxie.com>
2019-04-09 06:24:27 -0400

    rm tracks remove old dat files

--- src/app/albums/album.model.ts
+++ src/app/albums/album.model.ts
@@ -11,12 +11,4 @@ export interface Album {
   recording_location: string;
   duration: string;
   URL: string;
-  tracks?: Track[];
-}
-
-export interface Track {
-  id: number;
-  track_number: number;
-  title: string;
-  length: string;
 }
--- src/app/albums/albums.data.ts
+++
@@ -1,4 +0,0 @@
-// import { Album } from "./album.model";
-// import data from "../data/music-info.json";
-
-// export const ALBUMS: Album[] = data;
