a65d7179a294b85300c6379060ccc4f90b9a40ca
Judy Lipinski <judy@karmoxie.com>
2019-04-09 21:55:03 -0400

    updated to use the hard coded 3 albums

--- src/app/albums/album.model.ts
+++ src/app/albums/album.model.ts
@@ -4,19 +4,11 @@ export interface Album {
   album_name: string;
   genre: string;
   price: number;
-  currency: string;
+  currency?: string;
   on_sale: boolean;
   year: number;
   release_date: string;
   recording_location: string;
   duration: string;
-  url: string;
-  tracks: Track[];
-}
-
-export interface Track {
-  id: number;
-  track_number: number;
-  title: string;
-  length: string;
+  URL: string;
 }
